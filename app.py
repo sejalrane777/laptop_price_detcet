de5cd51c6ba2e78588a3d8cb20c0809b1d21c15e
Sejal Rane <55907599+sejalrane777@users.noreply.github.com>
2024-02-10 17:42:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 
 # import the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
+pipe = pd.read_pickle('pipe.pkl')
+df = pd.read_pickle('df.pkl')
 
 st.title("Laptop Predictor")
 
